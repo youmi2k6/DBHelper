c8dfcac712e103eb8f93c487332b2a9bbf6a5862
suxiang <651029594@qq.com>
2020-11-04 13:40:38 +0800

    完善

--- DBHelper/DBHelper/DBHelper.cs
+++ DBHelper/DBHelper/DBHelper.cs
@@ -1347,7 +1347,7 @@ namespace DBUtil
                         //}
                         //else
                         //{
-                        //    emitSetter = EmitSetter<T>(pro.Name);
+                        //    emitSetter = EmitUtil.EmitSetter<T>(pro.Name);
                         //    emitActions.Add(pro.Name, emitSetter);
                         //}
 
@@ -1801,41 +1801,6 @@ namespace DBUtil
         }
         #endregion
 
-        #endregion
-
-        #region EmitSetter
-        /// <summary>
-        /// EmitSetter
-        /// </summary>
-        public static Action<T, object> EmitSetter<T>(string propertyName)
-        {
-            var type = typeof(T);
-            var dynamicMethod = new DynamicMethod("EmitCall", null, new[] { type, typeof(object) }, type.Module);
-            var iLGenerator = dynamicMethod.GetILGenerator();
-
-            var callMethod = type.GetMethod("set_" + propertyName, BindingFlags.Instance | BindingFlags.IgnoreCase | BindingFlags.Public);
-            var parameterInfo = callMethod.GetParameters()[0];
-            var local = iLGenerator.DeclareLocal(parameterInfo.ParameterType, true);
-
-            iLGenerator.Emit(OpCodes.Ldarg_1);
-            if (parameterInfo.ParameterType.IsValueType)
-            {
-                iLGenerator.Emit(OpCodes.Unbox_Any, parameterInfo.ParameterType); // 如果是值类型，拆箱
-            }
-            else
-            {
-                iLGenerator.Emit(OpCodes.Castclass, parameterInfo.ParameterType); // 如果是引用类型，转换
-            }
-
-            iLGenerator.Emit(OpCodes.Stloc, local);
-            iLGenerator.Emit(OpCodes.Ldarg_0);
-            iLGenerator.Emit(OpCodes.Ldloc, local);
-
-            iLGenerator.EmitCall(OpCodes.Callvirt, callMethod, null);
-            iLGenerator.Emit(OpCodes.Ret);
-
-            return dynamicMethod.CreateDelegate(typeof(Action<T, object>)) as Action<T, object>;
-        }
         #endregion
 
     }
---
+++ DBHelper/DBHelper/Utils/EmitUtil.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Web;
+
+namespace DBUtil
+{
+    /// <summary>
+    /// Emit工具类
+    /// </summary>
+    public class EmitUtil
+    {
+        #region EmitSetter
+        /// <summary>
+        /// EmitSetter
+        /// </summary>
+        public static Action<T, object> EmitSetter<T>(string propertyName)
+        {
+            var type = typeof(T);
+            var dynamicMethod = new DynamicMethod("EmitCall", null, new[] { type, typeof(object) }, type.Module);
+            var iLGenerator = dynamicMethod.GetILGenerator();
+
+            var callMethod = type.GetMethod("set_" + propertyName, BindingFlags.Instance | BindingFlags.IgnoreCase | BindingFlags.Public);
+            var parameterInfo = callMethod.GetParameters()[0];
+            var local = iLGenerator.DeclareLocal(parameterInfo.ParameterType, true);
+
+            iLGenerator.Emit(OpCodes.Ldarg_1);
+            if (parameterInfo.ParameterType.IsValueType)
+            {
+                iLGenerator.Emit(OpCodes.Unbox_Any, parameterInfo.ParameterType); // 如果是值类型，拆箱
+            }
+            else
+            {
+                iLGenerator.Emit(OpCodes.Castclass, parameterInfo.ParameterType); // 如果是引用类型，转换
+            }
+
+            iLGenerator.Emit(OpCodes.Stloc, local);
+            iLGenerator.Emit(OpCodes.Ldarg_0);
+            iLGenerator.Emit(OpCodes.Ldloc, local);
+
+            iLGenerator.EmitCall(OpCodes.Callvirt, callMethod, null);
+            iLGenerator.Emit(OpCodes.Ret);
+
+            return dynamicMethod.CreateDelegate(typeof(Action<T, object>)) as Action<T, object>;
+        }
+        #endregion
+
+    }
+}
